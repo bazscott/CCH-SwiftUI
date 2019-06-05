2d19396cfc3691faad77d921d894c85e7a1e846b
Baz Scott <baz@bazscott.com>
2019-06-05 17:35:31 +1000

    Initial Commit

---
+++ CCH-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CCH-SwiftUI
+//
+//  Created by Baz Scott on 5/6/19.
+//  Copyright © 2019 Baz Scott. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
